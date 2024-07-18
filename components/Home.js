dc1b94325aa97992c6cb1cf47c7f65885c5c11d5
Felix <felix.dennery@gmail.com>
2024-07-18 17:31:41 +0200

    commit home tweets

--- components/Home.js
+++ components/Home.js
@@ -1,26 +1,47 @@
 import styles from '../styles/Home.module.css';
 import Head from 'next/head'
+import Image from 'next/Image'
+import { useState } from 'react'
+
 
 function Home() {
+
+const [newTweet, setNewTweet] = useState('')
+
   return (
-    <div>
+
+    <div className={styles.content}>
+    
       <Head> 
         <title> Hackatweet - Home</title> 
         </Head>
 
     <div className={styles.leftContainer}>
-      <span>Logo PlaceHolded</span>
+      <Image src='/twitter.png' alt="logo" width={100} height={100}/>
       <div className={styles.logoutContainer}>
         <span>User PlaceHolder</span>
-        <button>Logout</button>
+        <button className={styles.buttonHome}>Logout</button>
       </div>
     </div>
 
-    <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
+    <div className={styles.centerContainer}>
+    <h2> Home</h2>
+    <div className={styles.newTweet}>
+      <input className={styles.inputHome} type="text" placeholder='Write Something...' value={newTweet} onChange={(e) => setNewTweet(e.target.value)}/>
+      <span className={styles.carCount}>{newTweet.length}/280</span>
+      <button className={styles.buttonHome}>Tweet</button>
+    </div>
+ 
+    </div>
+
+    <div className={styles.rightContainer}>
+      <h2> Trends</h2>
+      <div className={styles.trendsContainer}>
+        <span>Trends Placeholder</span>
+      </div>
+
+    </div>
+       
     </div>
   );
 }
