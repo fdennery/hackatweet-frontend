ea9ccc2e2bb76d9e5464f511e3ceb16a2bd5bce9
Florian <florian.curier.57@gmail.com>
2024-07-18 17:36:52 +0200

    login

--- components/Login.js
+++ components/Login.js
@@ -1,9 +1,33 @@
 function Login() {
 
+const [signUpFirstname, setSignUpFirstname] = useState("")
+const [signUpUsername, setSignUpUsername] = useState("")
+const [signUpPassword, setSignUpPassword] = useState("")
+
+const [signInUsername, setSignUsername] = useState("")
+const [signInPassword, setSignInPassword] = useState("")
+
+const handleRegister = () => {
+    fetch("http://localhost:3000/users/signup", {
+        method: "POST",
+        headers: {"Content-Type": "application;json"},
+        body: JSON.string({ firstname: signUpFirstname, username: signUpUsername, password: signUpPassword}).then(response => response.json())
+        .then(data => {
+            console.log(data)
+        })
+    })
+}
+
     return (
         <div>
             <h1>Login</h1>
+            <p>Sign-up</p>
+            <input type="text" placeholder="Firstname" id="signUpFirstname" onChange={ (e) => setSignUpFirstname(e.target.value) } value={signUpFirstname} />
+            <input type="text" placeholder="Username" id="signUpUsername" onChange={ (e) => setSignUpUsername(e.target.value) } value={signUpUsername} />
+            <input type="text" placeholder="Password" id="signUpPassword" onChange={ (e) => setSignUpPassword(e.target.value) } value={signUpPassword} />
+            <button id= "register" onClick={() => handleRegister()}>Register</button>
         </div>
+        
     )
 }
 
