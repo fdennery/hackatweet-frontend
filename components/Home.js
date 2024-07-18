83b67de836829b2ce071eedee0008dea18403a8f
Felix <felix.dennery@gmail.com>
2024-07-18 15:49:31 +0200

    commit front FD

--- components/Home.js
+++ components/Home.js
@@ -1,9 +1,22 @@
 import styles from '../styles/Home.module.css';
+import Head from 'next/head'
 
 function Home() {
   return (
     <div>
-      <main className={styles.main}>
+      <Head> 
+        <title> Hackatweet - Home</title> 
+        </Head>
+
+    <div className={styles.leftContainer}>
+      <span>Logo PlaceHolded</span>
+      <div className={styles.logoutContainer}>
+        <span>User PlaceHolder</span>
+        <button>Logout</button>
+      </div>
+    </div>
+
+    <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
