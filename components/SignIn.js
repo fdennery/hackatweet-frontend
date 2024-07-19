ec39cc5054a18fdbe9b302175445b324951fa0b8
Florian <florian.curier.57@gmail.com>
2024-07-19 16:01:17 +0200

    front

--- components/SignIn.js
+++ components/SignIn.js
@@ -3,6 +3,8 @@ import styles from '../styles/Login.module.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { login } from '../reducers/user';
 import { useRouter } from 'next/router';
+import Image from 'next/image';
+import twitterImg from '../public/twitter.png';
 
 function SignIn() {
   const [signInUsername, setSignInUsername] = useState("");
@@ -29,16 +31,11 @@ function SignIn() {
 
   return (
     <div className={styles.authContainer}>
-      <div className={styles.authCard}>
-      <h2 className={`${styles.authTitle} ${styles['authTitle-dark']}`}>Connect to Hackatweet</h2>
-        <input type="text" placeholder="Username" onChange={(e) => setSignInUsername(e.target.value)} value={signInUsername}  className={`${styles.authInput} ${styles['authInput-dark']}`} />
-        <input
-  type="password"
-  placeholder="Password"
-  onChange={(e) => setSignInPassword(e.target.value)}
-  value={signInPassword}
-  className={`${styles.authInput} ${styles['authInput-dark']}`}
-/>
+      <div className={`${styles.authCard} ${styles['authCard-dark']}`}>
+        <Image src={twitterImg} alt="Twitter Logo" width={50} height={50} />
+        <h2 className={`${styles.authTitle} ${styles['authTitle-dark']}`}>Connect to Hackatweet</h2>
+        <input type="text" placeholder="Username" onChange={(e) => setSignInUsername(e.target.value)} value={signInUsername} className={`${styles.authInput} ${styles['authInput-dark']}`} />
+        <input type="password" placeholder="Password" onChange={(e) => setSignInPassword(e.target.value)} value={signInPassword} className={`${styles.authInput} ${styles['authInput-dark']}`} />
         <button onClick={handleConnection} className={styles.authButton}>Sign in</button>
       </div>
     </div>
--- components/SignUp.js
+++ components/SignUp.js
@@ -3,6 +3,8 @@ import styles from '../styles/Login.module.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { login } from '../reducers/user';
 import { useRouter } from 'next/router';
+import Image from 'next/image';
+import twitterImg from '../public/twitter.png';
 
 function SignUp() {
   const [signUpFirstname, setSignUpFirstname] = useState("");
@@ -18,18 +20,23 @@ function SignUp() {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ firstname: signUpFirstname, username: signUpUsername, password: signUpPassword })
-    }).then(response => response.json())
-      .then(data => {
+    })
+    .then(response => response.json())
+    .then(data => {
       if (data.result) {
-      dispatch(login({token: data.token, username:signUpUsername}))
-      router.push('/')
+        dispatch(login({token: data.token, username: signUpUsername}));
+        setSignUpFirstname('');
+        setSignUpUsername('');
+        setSignUpPassword('');
+        router.push('/');
       }
-})
-    }
+    });
+  };
 
   return (
     <div className={styles.authContainer}>
       <div className={`${styles.authCard} ${styles['authCard-dark']}`}>
+        <Image src={twitterImg} alt="Twitter Logo" width={50} height={50} />
         <h2 className={`${styles.authTitle} ${styles['authTitle-dark']}`}>Create your Hackatweet account</h2>
         <input type="text" placeholder="Firstname" onChange={(e) => setSignUpFirstname(e.target.value)} value={signUpFirstname} className={`${styles.authInput} ${styles['authInput-dark']}`} />
         <input type="text" placeholder="Username" onChange={(e) => setSignUpUsername(e.target.value)} value={signUpUsername} className={`${styles.authInput} ${styles['authInput-dark']}`} />
